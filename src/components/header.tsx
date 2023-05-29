6125d750f17fb07f79ab6e3d4cf0926f8ed265e0
Wanuja Ranasinghe <63337783+Wanuja97@users.noreply.github.com>
2023-05-29 19:11:50 +0530

    active tab animated in sidenavbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,17 +3,12 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <div aria-label="Page Header" className="bg-gray-100">
+            <div aria-label="Page Header" className="bg-gray-200">
                 <div className="w-screen px-4 py-8 sm:px-6 lg:px-8">
                     <div className="mt-8">
                         <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-                            Welcome Back, Barry! 👋
+                            Welcome Back, Wanuja! 👋
                         </h1>
-
-                        <p className="mt-1.5 text-sm text-gray-500">
-                            Your website has seen a 52% increase in traffic in the last month. Keep
-                            it up! 🚀
-                        </p>
                     </div>
                 </div>
             </div>
